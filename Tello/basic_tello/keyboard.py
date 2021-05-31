526409434abc97c62a9715cad72d1517103fb0e8
FLY-CODE77 <lightyear94122@gmail.com>
2021-05-31 10:39:55 +0900

    typo change all function work

--- Tello/basic_tello/keyboard.py
+++ Tello/basic_tello/keyboard.py
@@ -10,20 +10,20 @@ def getKey(keyName):
     keyInput = pygame.key.get_pressed()
     myKey = getattr(pygame, "K_{}".format(keyName))
 
-    if keyInpit[myKey]:
+    if keyInput[myKey]:
         ans = True
-    pygame.display.updata()
+    pygame.display.update()
 
     return ans
 
-def main()
+def main():
     if getKey("LEFT"):
         print("Left key pressed")
     
     if getKey("RIGHT"):
         print("Right key pressed")
 
-if __name__ == "__main_":
+if __name__ == "__main__":
     init()
     while True:
         main()
